982ba597f4ccf6a61918e7868ad7f0e70de4735b
TrakeLean <91937397+TrakeLean@users.noreply.github.com>
2025-04-03 14:51:30 +0200

    Update cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,7 @@ const { program } = require('commander');
 const setup = require('../src/commands/setup');
 const create = require('../src/commands/create');
 const solve = require('../src/commands/solve');
+const leaderboard = require('../src/commands/leaderboard');
 const updateReadme = require('../src/commands/updateReadme');
 const packageInfo = require('../package.json');
 
@@ -26,6 +27,11 @@ program
   .command('solve')
   .description('Check or set flag for completed task')
   .action(solve);
+
+program
+  .command('leaderboard')
+  .description('Generate a leaderboard of challenge solvers')
+  .action(leaderboard);
 
 program
   .command('update')
