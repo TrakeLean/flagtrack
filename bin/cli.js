9088646f6d671d6c1c8c2a172eb8c64575d35612
TrakeLean <91937397+TrakeLean@users.noreply.github.com>
2025-04-03 13:58:17 +0200

    Create cli.js

---
+++ bin/cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+const setup = require('../src/commands/setup');
+const newtask = require('../src/commands/newtask');
+const updateReadme = require('../src/commands/updateReadme');
+const packageInfo = require('../package.json');
+
+program
+  .name('flagtrack')
+  .description('CTF Progress Tracker and Task Management Tool')
+  .version(packageInfo.version);
+
+program
+  .command('setup')
+  .description('Set up a new CTF project with required configuration')
+  .action(setup);
+
+program
+  .command('newtask')
+  .description('Create a new CTF challenge task')
+  .action(newtask);
+
+program
+  .command('update')
+  .description('Update the README with the current CTF progress')
+  .action(updateReadme);
+
+program.parse(process.argv);
+
+// If no command is provided, show help
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
