3b7ff381615fa2a2713176b8e3d9d399a771a6bd
TrakeLean <91937397+TrakeLean@users.noreply.github.com>
2025-04-03 14:51:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 const setup = require('./src/commands/setup');
 const create = require('./src/commands/create');
 const solve = require('./src/commands/solve');
+const leaderboard = require('./src/commands/leaderboard');
 const updateReadme = require('./src/commands/updateReadme');
 const { loadConfig } = require('./src/utils/configManager');
 const gitHelpers = require('./src/utils/gitHelpers');
@@ -16,6 +17,7 @@ module.exports = {
     setup,
     create,
     solve,
+    leaderboard,
     updateReadme
   },
   utils: {
